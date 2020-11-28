824336808c61f5b21ea57b7e697df8f5bbe1ce69
Giuseppe Toto <giuseppe.toto@gmail.com>
2020-11-28 17:54:01 +0100

    add basecode the kids game developed from Jason Weimann and created a test scene

---
+++ Assets/Scripts/ClickableLetter.cs
@@ -0,0 +1,37 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ClickableLetter : MonoBehaviour, IPointerClickHandler
+{
+    char _randomLetter;
+    bool _upperCase;
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log($"Clicked on letter {_randomLetter}");
+        if (_randomLetter == GameController.Instance.Letter)
+        {
+            GetComponent<TMP_Text>().color = Color.green;
+            enabled = false;
+            GameController.Instance.HandleCorrectLetterClick(_upperCase);
+        }
+    }
+
+    public void SetLetter(char letter)
+    {
+        enabled = true;
+        GetComponent<TMP_Text>().color = Color.white;
+        _randomLetter = letter;
+
+        if (Random.Range(0, 100) > 50)
+        {
+            _upperCase = false;
+            GetComponent<TMP_Text>().text = _randomLetter.ToString();
+        }
+        else
+        {
+            _upperCase = true;
+            GetComponent<TMP_Text>().text = _randomLetter.ToString().ToUpper();
+        }
+    }
+}
---
+++ Assets/Scripts/RemainingCounterText.cs
@@ -0,0 +1,10 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class RemainingCounterText : MonoBehaviour
+{
+    internal void SetRemaining(int remaining)
+    {
+        GetComponent<TMP_Text>().text = "x" + remaining;
+    }
+}
